2cbd49e69240473f27a0fbe51c3df32fd68b3262
Harsha03D <harsha2k03@gmail.com>
2025-11-12 19:11:42 +0530

    updated message in app.js

--- CD_Jenkins_Docker/app.js
+++ CD_Jenkins_Docker/app.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Jenkins + Docker Pipeline 🚀');
+  res.end('Welcome to OneData Assessment - Node.js Application deployed via Jenkins and Docker. Continuous Integration & Deployment completed successfully.');
 });
 
 server.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('OneData Assessment App deployed successfully using Jenkins CI/CD Pipeline. Running on port 3000.');
 });
